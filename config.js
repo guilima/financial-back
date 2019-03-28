7eedbdb8146a6941ad6c74a798af372cf4593f93
guilima <gui.lima30@gmail.com>
2019-03-27 22:42:32 -0300

    feat(config/mongodb): Add username and password variable

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ module.exports = {
   db: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME_DEVELOPMENT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
   },
   port: process.env.PORT
 };
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,9 +1,10 @@
 const mongoClient = require("mongodb").MongoClient;
-const { db: {host, database } } = require('./../config');
+const { db: {host, database, user, password } } = require('./../config');
 
 module.exports = (async () => {
+  const url = password ? `mongodb://${user}:${password}@${host}/${database}` : `mongodb://${host}/${database}`;
   const client = await mongoClient.connect(
-    host,
+    url,
     { useNewUrlParser: true }
   );
   const db = client.db(database);
