f0c922e81b231bd8f2876cfb7ac56e723db508b2
Guilherme Lima <gui.lima30@gmail.com>
2019-07-29 22:08:20 -0300

    feat(@koa/cors): Add cors for POST method

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
 import routes from './src/routes/routes';
 import { port } from './config';
 
 const app = new Koa();
 app.use(bodyParser())
+  .use(cors({
+    origin: 'https://guilima.github.io',
+    allowHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+    credentials: true
+  }))
   .use(routes.routes())
   .use(routes.allowedMethods());
 
