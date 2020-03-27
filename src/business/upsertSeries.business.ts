2f2b6383d7044b14d70366300569ff28b78475d8
Guilherme Lima <gui.lima30@gmail.com>
2020-03-26 22:13:32 -0300

    feat(upsertSeries.business): Include alphaService for ibovespa data
    The central bank stopped providing data from time series ibovespa, as well gold.
    Implemented alphaService to get ibovespa monthly series data.
    Need to research some API for the gold monthly data.

--- src/business/upsertSeries.business.ts
+++ src/business/upsertSeries.business.ts
@@ -1,5 +1,6 @@
 import { upsertSeries } from "@data/query";
 import CentralBankAPI from "@services/centralBank.service";
+import AlphaVantageAPI from "@services/alphaVantage.service";
 
 function parseId(id: string): string {
   const parse = {
@@ -21,12 +22,49 @@ function parseDateCentralBankResponse(date: string): string {
   return new Date(`${year}-${month.padStart(2, "0")}`).toISOString();
 }
 
+function parseAlphaId(arr: string[], id: string): string[] {
+  const parse = {
+    '7832': '^BVSP'
+  }
+  return parse[id] ? arr.concat(parse[id]) : arr;
+}
+
+function percentage(number1: number, number2: number): number {
+  const total = ( ( ( number2 * 100 ) / number1 ) - 100 ) * 1000;
+  return Number( ( ( total < 0 ? Math.round(total) : Math.ceil(total) ) / 1000 ).toFixed(2) );
+}
+
 export default async (ctx) => {
   const { idGroup, date: { initial, end } } = ctx.request.body;
   const centralBankAPI = new CentralBankAPI();
-
+  const alphaVantageAPI = new AlphaVantageAPI();
+  const idGroupCentralBank = idGroup.filter(id => parseId(id) !== "Ibovespa");
+  const idGroupAlpha = idGroup.reduce(parseAlphaId, []);
   try {
-    const getValoresSeriesResponse = await centralBankAPI.getValoresSeries({ idGroup, initial, end });
+    const getAlphaMonthlyResponse = await alphaVantageAPI.getMonthlySeries(idGroupAlpha);
+    const alpha = getAlphaMonthlyResponse.map( item => {
+      const dates = Object.keys(item["Monthly Time Series"]);
+      const firstDateIndex = dates.findIndex(date => new RegExp(end.substring(0, 7)).test(date));
+      if(firstDateIndex === -1) ctx.throw(500, `Ano e mês ${end} não encontrado na serie temporal mensal (getAlphaMonthlyResponse)`);
+      const lastDateIndex = dates.findIndex(date => new RegExp(initial.substring(0, 7)).test(date));
+      if(lastDateIndex === -1) ctx.throw(500, `Ano e mês ${initial} não encontrado na serie temporal mensal (getAlphaMonthlyResponse)`);
+      const datesToInitial = dates.slice(firstDateIndex, lastDateIndex + 2)
+      return {
+        id: 7832,
+        name: parseId("7832"),
+        series: datesToInitial.reduce((arr, date, index) => {
+          const currentDate = item["Monthly Time Series"][date];
+          const previousDate = item["Monthly Time Series"][datesToInitial[index - 1]];
+          return previousDate ? arr.concat({
+            date: new Date(datesToInitial[index - 1].substring(0, 7)).toISOString(),
+            value: currentDate ? percentage(currentDate["4. close"], previousDate["4. close"]) : null,
+            disabled: false
+          }) : arr;
+        }, [])
+      };
+    });
+
+    const getValoresSeriesResponse = await centralBankAPI.getValoresSeries({ idGroup: idGroupCentralBank, initial, end });
     const json = getValoresSeriesResponse.map(serie => {
       return {
         id: serie.ID,
@@ -41,7 +79,7 @@ export default async (ctx) => {
       }
     });
     const param = {
-      items: json,
+      items: json.concat(alpha),
       date: {
         initial: new Date(initial).toISOString(),
         end: new Date(end).toISOString()
