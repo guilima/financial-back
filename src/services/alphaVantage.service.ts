e709eac53c598744b0b2a89f18270df0ea61aaf1
Guilherme Lima <gui.lima30@gmail.com>
2020-03-26 22:13:13 -0300

    feature(alphaService): Include alpha vantange service and api key

---
+++ src/services/alphaVantage.service.ts
@@ -0,0 +1,36 @@
+import Https from 'https';
+import { alphaApiKey } from '../../config';
+// import { IGetValoresSeriesJSONResponse } from "@schema/series.schema";
+
+export default class AlphaVantageAPI {
+  url: string;
+  alphaApikey: string;
+  constructor() {
+    this.url = 'https://www.alphavantage.co/query';
+    this.alphaApikey = alphaApiKey;
+  }
+
+  async getMonthlySeries(symbols: string[]) {
+    try {
+      const body = symbols.map(symbol => {
+        return new Promise((resolve, reject) => {
+          Https.get(`${this.url}?function=TIME_SERIES_MONTHLY&symbol=${symbol}&apikey=${this.alphaApikey}`, (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+              data += chunk;
+            }).on('end', () => {
+              const responseParse = JSON.parse(data);
+              responseParse["Meta Data"] ? resolve(responseParse) : reject(responseParse);
+            });
+          }).on("error", (err) => {
+            reject(err);
+          });
+        });
+      });
+
+      return await Promise.all(body);
+    } catch(err) {
+      return err;
+    }
+  }
+}
