cc9c14cbed1776e0a37f5d2ad0c59d8ccd06bdf1
Guilherme Lima <gui.lima30@gmail.com>
2020-04-01 00:32:10 -0300

    feat(moment): Add dependency and change business date with .utc()

--- src/business/getSeries.business.ts
+++ src/business/getSeries.business.ts
@@ -1,13 +1,13 @@
 import { getSeries } from "@data/query";
 import { Context } from "koa";
+import { utc } from 'moment';
 
 export default async (ctx: Context) => {
   const { idGroup, dateInitial, dateEnd } = ctx.request.body;
-  const [iYear, iMonth] = dateInitial.split("-");
   const params = {
     series: idGroup.map(_id => ( { _id } )),
     date: {
-      initial: new Date(`${iYear}-${iMonth}-01`).toISOString(),
+      initial: utc(dateInitial).startOf("month").toISOString(),
       end: new Date(dateEnd).toISOString()
     }
   };
