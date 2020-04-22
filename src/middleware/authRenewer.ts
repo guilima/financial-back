b81aebb670cfb6400800914a5fa1f189fb02bf32
Guilherme Lima <gui.lima30@gmail.com>
2020-04-22 16:04:52 -0300

    refactor(authRenewer): Add detail on refresh token error

--- src/middleware/authRenewer.ts
+++ src/middleware/authRenewer.ts
@@ -5,30 +5,33 @@ import { Context, Next, Middleware } from 'koa';
 const jwToken = new JwToken();
 
 export default async (ctx: Context, next: Next): Promise<Middleware> => {
+    const redisClient = ctx.session._sessCtx.store.client;
+    const tokenAccess = ctx.cookies.get('tokenAccess', {signed: true});
+    const tokenRefresh = ctx.session.tokenRefresh;
     try {
         const originalError = ctx.state.jwtOriginalError;
         if (!originalError) {
             return next();
         }
-        const redisClient = ctx.session._sessCtx.store.client;
-        const tokenAccess = ctx.cookies.get('tokenAccess', {signed: true});
-        const tokenRefresh = ctx.session.tokenRefresh;
-        if (originalError.name !== 'TokenExpiredError' || !tokenAccess || !tokenRefresh) {
-            if(tokenRefresh) {
-                await redisClient.zadd('blacklist', (jwToken.decode(tokenRefresh).exp * 1000), tokenRefresh);
-                await redisClient.zremrangebyscore('blacklist', '-inf', Date.now());
-            }
+        if (originalError.name !== 'TokenExpiredError') {
             ctx.throw(401, "Authentication Error", { originalError });
         }
-
+        if(!tokenRefresh) {
+            originalError.detail = "Invalid Refresh Token";
+            ctx.throw(401, "Authentication Error", { originalError });
+        }
         const isRefreshTokenRevoked = Number.isInteger(await redisClient.zrank('blacklist', tokenRefresh));
         if (isRefreshTokenRevoked) {
+            originalError.detail = "Revoked Refresh Token";
             ctx.throw(401, "Authentication Error", { originalError });
         }
         const isRefreshTokenExpired = new Date() > new Date(jwToken.decode(tokenRefresh).exp * 1000);
         if (isRefreshTokenExpired) {
+            originalError.detail = "Expired Refresh Token";
             ctx.throw(401, "Authentication Error", { originalError });
         }
+        await redisClient.zadd('blacklist', (jwToken.decode(tokenRefresh).exp * 1000), tokenRefresh);
+        await redisClient.zremrangebyscore('blacklist', '-inf', Date.now());
         const { sub: uid, name } = jwToken.decode(tokenAccess);
         const newTokenAcess = jwToken.sign({
             sub: uid,
@@ -40,6 +43,10 @@ export default async (ctx: Context, next: Next): Promise<Middleware> => {
         ctx.session.tokenRefresh = newTokenRefresh;
         return await next();
     } catch (err) {
+        if(!err.originalError.detail && tokenRefresh) {
+            await redisClient.zadd('blacklist', (jwToken.decode(tokenRefresh).exp * 1000), tokenRefresh);
+            await redisClient.zremrangebyscore('blacklist', '-inf', Date.now());
+        }
         ctx.cookies.set('tokenAccess');
         delete ctx.session.tokenRefresh;
         throw err;
