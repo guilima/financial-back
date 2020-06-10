1bcb0527d5171fda3b5dd38af38ae39a03beed22
Guilherme Lima <gui.lima30@gmail.com>
2020-06-10 18:52:45 -0300

    fix(migrations): Prevent nullable on update for user/logins table

--- migrations/20200523144238_logins.ts
+++ migrations/20200523144238_logins.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<any> {
         table.increments('id');
         table.specificType('password_hash', 'char(128)');
         table.binary('password_salt');
-        table.specificType('logged_at', 'timestamp').defaultTo(knex.fn.now());
+        table.specificType('logged_at', 'timestamp').defaultTo(knex.fn.now()).notNullable();
         table.specificType('ip_address', 'inet');
         table.integer('user_id').references('id').inTable('users');
     });
