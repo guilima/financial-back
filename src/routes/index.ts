e8ec7a18adb73888979c6fe5098e0d42018caf34
Guilherme Lima <gui.lima30@gmail.com>
2020-08-12 19:34:08 -0300

    feat(series): Remove module - Remove mongodb connection - Remove alphaVantage and centralBank service - Remove dependencies (mongodb, xml, soap)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,12 @@
 import Router from '@koa/router';
 import Wallet from './wallet.routes';
 import Auth from './auth.routes';
-import Series from './series.routes';
 import User from './user.routes';
 import { DefaultState, Context } from 'koa';
 const router = new Router<DefaultState, Context>();
 router
     .use(User.routes())
     .use(Auth.routes())
-    .use(Wallet.routes())
-    .use(Series.routes());
+    .use(Wallet.routes());
 
 export default router;
