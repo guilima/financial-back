dcaa37ec7e65a7566f150cdde0ad4fec9cd5c016
guilima <gui.lima30@gmail.com>
2021-04-13 19:18:32 -0300

    chore(config): Remove legacy env variables

--- config.ts
+++ config.ts
@@ -1,10 +1,8 @@
 export const port = process.env.PORT;
 export const appCors = process.env.APP_CORS;
-export const mongodbURI = process.env.MONGODB_URI;
 export const postgresURI = process.env.POSTGRES_URI;
 export const redisURI = process.env.REDIS_URI;
 export const jwtSecret = process.env.JWT_SECRET;
 export const jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
-export const alphaApiKey = process.env.ALPHA_API_KEY;
 export const cookieKeys = [process.env.COOKIE_KEYS];
 export const googleRecaptchaV2ApiKey = process.env.GOOGLE_RECAPTCHA_V2_API_KEY;
